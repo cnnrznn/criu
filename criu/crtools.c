32fc4e744fe3112fc630df25689ed849d0004f86
Ubuntu <ubuntu@ip-10-233-84-247.ec2.internal>
2016-10-26 15:14:54 +0000

    Renamed pico-cache to pico-cache-dump. pico-cache-restore will have different semantics and therefore deserves a unique name

--- criu/crtools.c
+++ criu/crtools.c
@@ -282,7 +282,7 @@ int main(int argc, char *argv[], char *envp[])
 		{ "display-stats",		no_argument,		0, 1086 },
 		{ "meta-dump",			no_argument,		0, 2000 },
 		{ "disk-serve",			no_argument,		0, 2001 },
-        { "pico-cache",         required_argument,  0, 2002 },
+        { "pico-cache-dump",         required_argument,  0, 2002 },
 		{ },
 	};
 
@@ -608,7 +608,7 @@ int main(int argc, char *argv[], char *envp[])
             opts.disk_serve = true;
             break;
         case 2002:
-            opts.pico_cache = optarg;
+            opts.pico_cache_dump = optarg;
             break;
 		case 'V':
 			pr_msg("Version: %s\n", CRIU_VERSION);
